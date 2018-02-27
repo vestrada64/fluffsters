8608909061c81b3e987ee1f739b82602b33690aa
Vanessa Estrada <vestrada64@gmail.com>
2018-02-27 12:43:54 -0800

    clean up login and signup forms

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -47,7 +47,7 @@ class LoginForm extends Component {
           <div className="form-group">
             <div className="col-sm-12 text-center">
               <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
-              <Link to='/'>Cancel</Link>
+              <Link className ="cancelbtn" to='/'>Cancel</Link>
             </div>
           </div>
         </form>
