b401c6db7d33e8e02ffb489638f178914a2d2a5e
Vanessa Estrada <vestrada64@gmail.com>
2018-02-28 14:57:17 -0800

    add home button

--- src/pages/PuppyPage/PuppyPage.jsx
+++ src/pages/PuppyPage/PuppyPage.jsx
@@ -6,16 +6,15 @@ import './../ShowPuppyPage/ShowPuppyPage';
 
 const PuppyPage = ({ puppies }, props) => (
     <div>
-        <h1>All Puppies</h1>
+        <header>All Puppies</header>
         <Link className ="homebtn"to='/'>Home</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+        <Link to='/aboutus'>What is this App about?</Link>
         <ul>
         { puppies.map((puppy, idx) => <li key={idx}><Link to={`/puppies/${idx}`}>{puppy.name}<img className="showpuppy" src={puppy.image_url}></img></Link></li>) }
-
         </ul>
     </div>
         
 )
-
-
 
 export default PuppyPage;
