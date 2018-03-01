adfe1c1d538fd2fa31264d75ba95736dade95eed
Vanessa Estrada <vestrada64@gmail.com>
2018-02-28 17:01:04 -0800

    add redirect to gallery

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -23,7 +23,7 @@ class LoginForm extends Component {
     userService.login(this.state)
       .then(() => {
         this.props.handleLogin();
-        this.props.history.push('/');
+        this.props.history.push('/puppies');
       })
       // invalid credentials - don't alert in YOUR app :)
       .catch(err => alert('Invalid Credentials!'));
